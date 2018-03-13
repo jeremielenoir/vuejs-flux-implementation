ae7ffbf63bc543274a84f14e5d45920347f4e956
Van Tien NGUYEN <van-tien.nguyen@hotmail.fr>
2018-03-13 14:12:37 +0100

    Navbar searchbar

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-light bg-light justify-content-between">
+  <a class="navbar-brand">Navbar</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
     <h1>Todo List</h1>
     <router-view/>
   </div>
@@ -18,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
