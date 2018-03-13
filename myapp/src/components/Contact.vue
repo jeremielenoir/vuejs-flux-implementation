bcb0d55e2a8cb843d57d256af1880cf6022aa784
Van Tien NGUYEN <van-tien.nguyen@hotmail.fr>
2018-03-13 17:43:57 +0100

    event emit on parent child

--- myapp/src/components/Contact.vue
+++ myapp/src/components/Contact.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1>Page contact</h1>
+<div class="container">
+    <div id="counter-event-example">
+        <p>{{ total }}</p>
+        <button-counter v-on:increment="incrementTotal"></button-counter>
+        <button-counter v-on:increment="incrementTotal"></button-counter>
+    </div>
+</div>
 </template>
+<script>
+import Vue from 'vue'
+
+
+Vue.component('button-counter', {
+  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  },
+})
+
+new Vue({
+  el: '#counter-event-example',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
+  }
+})
+</script>
--- myapp/src/components/TodoList.vue
+++ myapp/src/components/TodoList.vue
@@ -72,5 +72,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.hello {
+  margin-top: 20px;
+}
 </style>
